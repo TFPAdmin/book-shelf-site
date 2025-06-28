e7a73a2543d8aad43f0a5befec2f456c5b7cf6d5
Corey H <github@thefamilyplan.xyz>
2025-06-27 16:07:31 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src', // Set 'src' folder as the root
   plugins: [react()],
   build: {
-    outDir: '../dist', // Output directory relative to 'src'
-    emptyOutDir: true, // Clears output before build
-  },
+    outDir: 'dist',       // Output in root-level /dist
+    emptyOutDir: true
+  }
 })
